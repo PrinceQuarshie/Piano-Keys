0c302542c6b63dcbde032685bee23eea96ca722b
PrinceQuarshie <prince.quarshie@amalitech.org>
2022-04-15 18:22:47 +0000

    main.js | final part

--- main.js
+++ main.js
@@ -80,3 +80,45 @@ nextTwo.onclick = function() {
 
   lastLyric.style.display = 'inline-block'
 }
+
+
+// Write anonymous event handler property and function for the third progress button
+nextThree.onclick = function() {
+   startOver.hidden = false;
+    nextThree.hidden = true;
+
+document.getElementById('word-one').innerHTML = 'HAP-'
+document.getElementById('word-two').innerHTML = 'PY'
+document.getElementById('word-three').innerHTML = 'BIRTH'
+document.getElementById('word-four').innerHTML = 'DAY'
+document.getElementById('word-five').innerHTML = 'TO'
+document.getElementById('word-six').innerHTML = 'YOU'
+
+document.getElementById('letter-note-one').innerHTML = 'F'
+document.getElementById('letter-note-two').innerHTML = 'F'
+document.getElementById('letter-note-three').innerHTML = 'E'
+document.getElementById('letter-note-four').innerHTML = 'C'
+document.getElementById('letter-note-five').innerHTML = 'D'
+document.getElementById('letter-note-six').innerHTML = 'C'
+
+lastLyric.style.display = 'none'
+
+}
+
+// This is the event handler property and function for the startOver button
+startOver.onclick = function() {
+  nextOne.hidden = false;
+  startOver.hidden = true;
+   document.getElementById('word-one').innerHTML = 'HAP-';
+  document.getElementById('letter-note-one').innerHTML = 'G';
+  document.getElementById('word-two').innerHTML = 'PY';
+  document.getElementById('letter-note-two').innerHTML = 'G';
+  document.getElementById('word-three').innerHTML = 'BIRTH-';
+  document.getElementById('letter-note-three').innerHTML = 'A';
+  document.getElementById('word-four').innerHTML = 'DAY';
+  document.getElementById('letter-note-four').innerHTML = 'G';
+  document.getElementById('word-five').innerHTML = 'TO';
+  document.getElementById('letter-note-five').innerHTML = 'C';
+  document.getElementById('word-six').innerHTML = 'YOU!';
+  document.getElementById('letter-note-six').innerHTML = 'B';
+}
